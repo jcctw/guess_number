b0291ce5fde02ecf79d68a44d8d475a3f1635e84
jcctw <88425249+jcctw@users.noreply.github.com>
2021-08-12 17:39:13 +0800

    Update guess.py
    Add counter

--- guess.py
+++ guess.py
@@ -2,18 +2,20 @@
 
 import random
 r = random.randint(1, 100)
+count = 1 #number of guess
 
 while True:
 	num = input('Please guess a number between 1 and 100:') #string
 	num = int(num) #casting
 	if num == r:
 		print('Congrats! You are right!')
+		print('You have guessed', count, 'time(s).')
 		break
 	elif num > r:
 		print('Your guess is larger than the number!')
-		print('Try again...')
 	else:
 		print('Your guess is smaller than the number!')
-		print('Try again...')
+	print('You have guessed', count, 'time(s).')
+	count = count + 1 
 
 
